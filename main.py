56fcf57c8c48b3fb80fed69c1590463596bb7fae
Vladimir <vlad_e@rambler.ru>
2024-02-20 16:13:57 +0300

    Подключение библиотеки aiohttp

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
+# import requests
+import  aiohttp
+import  asyncio
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def make_send_message_url(token, chat_id, text):
+    base_url = 'https://api.telegram.org'
+    return f'{base_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_updates_url(token, offset=None):
+    base_url = 'https://api.telegram.org'
+    return f'{base_url}/bot{token}/getUpdates?offset={offset}'
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+async def answer_to_message(session, token, update):
+    chat_id = update['message']['chat']['id']
+    url = make_send_message_url(
+        token, chat_id, 'Наш ответ!'
+    )
+    async with session.get(url) as response:
+        result = await response.json()
+        print(result)
+
+
+async def main():
+    token = '6101955433:AAFIqsV8NRXr6S05Uajr265hJUUbmv-jrfg'
+    offset = -1
+#    url = make_send_message_url(
+#        '6101955433:AAFIqsV8NRXr6S05Uajr265hJUUbmv-jrfg',
+#        1456359683,
+#        'Привет!'
+#    )
+    async with aiohttp.ClientSession() as session:
+        while True:
+            url = get_updates_url(token, offset=offset)
+            async with session.get(url) as response:
+                result = await response.json()
+#               user_first_name = result['result']['chat']['first_name']
+#               print(f'{user_first_name} отправил сообщение')
+                for update in result['result']:
+                    await answer_to_message(session, token, update)
+                    offset = update['update_id'] + 1
+            await asyncio.sleep(1)
+
+#asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
+
+# response = requests.get(url)
+# result = response.json()
+# print(result)
+# first_name = result['result']['from']['first_name']
+# print(first_name)
