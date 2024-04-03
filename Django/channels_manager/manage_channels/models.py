b8c6a12cd92a932bb306453b9730697223c4f2f3
Vladimir <vlad_e@rambler.ru>
2024-04-03 17:30:51 +0300

    Добавление моделей 7-14

--- Django/channels_manager/manage_channels/models.py
+++ Django/channels_manager/manage_channels/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    is_enrolled = models.BooleanField(default=False)
+    course1_id = models.IntegerField()
+    grade1_id = models.IntegerField()
+    course2_id = models.IntegerField()
+    grade2_id = models.IntegerField()
+    course3_id = models.IntegerField()
+    grade3_id = models.IntegerField()
+    registered_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'users'
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'courses'
+
+
+class Grade(models.Model):
+    grade = models.IntegerField()
+
+    class Meta:
+        db_table = 'grades'
